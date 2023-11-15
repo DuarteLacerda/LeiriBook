529b74bf15bfa23245a01f1268e5005503eead6d
Portátil Duarte <duartelacerda4@gmail.com>
2023-11-15 11:40:45 +0000

    Nav & Footer
    15/11/23

---
+++ leiribook/resources/views/layout/parcial/nav.blade.php
@@ -0,0 +1,53 @@
+<nav>
+    <div class="navbar">
+        <div class="container nav-container">
+            <input class="checkbox" type="checkbox" name="" id="" />
+            <div class="hamburger-lines">
+                <span class="line line1"></span>
+                <span class="line line2"></span>
+                <span class="line line3"></span>
+            </div>
+            <div class="nave">
+                <ul>
+                    <li><a href="#"><img src="images/logo-text.svg" alt="logotipo" width="140px"></a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Bilioteca Virtual</a></li>
+                    <li><a href="#">Eventos</a></li>
+                    <li><a href="#">Contactos</a></li>
+                </ul>
+            </div>
+            <div class="logo">
+                <!-- Login/registo -->
+                <a class="login" href="#">
+                    Login</a>
+                <a class="register" href="#">
+                    Registo</a>
+                </a>
+                <button type="button" class="search" id="search"><i
+                        class="fa-solid fa-magnifying-glass"></i></button>
+            </div>
+            <div class="menu-items">
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Bilioteca Virtual</a></li>
+                <li><a href="#">Eventos</a></li>
+                <li><a href="#">Contactos</a></li>
+                </a></li>
+            </div>
+        </div>
+    </div>
+</nav>
+
+<!-- The Modal -->
+<div id="navModal" class="modal">
+    <!-- Modal content -->
+    <div class="modal-content">
+        <div class="modal-search">
+            <form><input class="border-0" type="text" name="search" placeholder="Search..">
+                <button id="searchModal" class=" b-modal" type="submit"><i
+                        class="fa-solid fa-magnifying-glass"></i></button>
+            </form>
+        </div>
+
+    </div>
+</div>
