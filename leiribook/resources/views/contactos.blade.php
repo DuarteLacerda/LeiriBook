2f1b9b0245bb07032d99d27604dfdfb90014fb01
daniel <danielribeiro290404@hotmail.com>
2023-11-15 13:29:58 +0000

    pagina contactos quase terminada

--- leiribook/resources/views/contactos.blade.php
+++ leiribook/resources/views/contactos.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.master')
 @section('title', 'LeiriBook-Contactos')
 @section('styles')
-<link rel="stylesheet" href="{{ asset('css/ribeiro.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/ribeiro.css') }}">
 @endsection
 
 @section('content')
@@ -26,5 +26,11 @@
                 <input type="submit" value="Inscreva-se">
             </div>
         </form>
+    </div>
+    <div id="location">
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d370.227918486206!2d-8.842207327272678!3d39.72691130291961!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd220ca9efda704d%3A0x5156ce4c50e3195f!2sRua%20dos%20Parceiros%202104%2C%202400-497%20Parceiros!5e1!3m2!1spt-PT!2spt!4v1700053947059!5m2!1spt-PT!2spt"
+            width="68%" height="600" style="border:0;" allowfullscreen="" loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 @endsection
