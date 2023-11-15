8fcd00a83e5a6dff551b55401529e95be757fa24
daniel <danielribeiro290404@hotmail.com>
2023-11-15 10:37:57 +0000

    eu cometo ASSASSINATO

---
+++ leiribook/resources/views/layout/master.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title', 'Your Default Title')</title>
+    <!-- Include your stylesheets, scripts, or other head elements here -->
+</head>
+<body>
+    <header>
+        @include('nav') <!-- Include your navigation here -->
+    </header>
+    <main>
+        @yield('content')
+    </main>
+    <footer>
+        @include('footer') <!-- Include your footer here -->
+    </footer>
+    <!-- Include your scripts, such as JavaScript libraries, at the end of the body -->
+    @yield('scripts')
+</body>
+</html>
