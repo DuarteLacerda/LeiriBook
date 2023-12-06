896babb0f91fb35591fed5f2625322c938c454eb
paulo <pauloandeleal@gmail.com>
2023-12-06 14:02:42 +0000

    página pedidos

---
+++ leiribook/resources/views/pedido_livro.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.master')
+@section('title', 'Pedido para Livro')
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('css/paulo.css') }}">
+@endsection
+@section('content')
+
+    <div class="text-section">
+        <h1 class="title">Pedido de Livro</h1>
+        <h2 class="desc-title">Não encontra um livro em particular? Faça um pedido para adicionarmos o livro em falta</h2>
+    </div>
+
+    
+
+
+@endsection
