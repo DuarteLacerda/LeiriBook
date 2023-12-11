d4aac70396f409b0ae3265eb40bdfb95f3765441
daniel <danielribeiro290404@hotmail.com>
2023-12-11 17:29:46 +0000

    segunda versao atualizada da pagina biblioteca

--- leiribook/resources/views/biblioteca.blade.php
+++ leiribook/resources/views/biblioteca.blade.php
@@ -26,7 +26,7 @@
                     <p>Não há livros</p>
                 @endforelse
 
-                    <div class="book">
+                    <!--<div class="book">
                         <div class="cover">
                             <img src="https://alysbcohen.files.wordpress.com/2015/01/little-princess-book-cover.jpg">
                         </div>
@@ -35,68 +35,7 @@
                                 <span class="author">Frances Hodgson Burnett</span>
                             </p>
                         </div>
-                    </div>
-                    <div class="book">
-                        <div class="cover">
-                            <img src="http://www.publishersweekly.com/images/data/ARTICLE_PHOTO/photo/000/028/28129-1.JPG">
-                        </div>
-                        <div class="description">
-                            <p class="title">Roughing It<br>
-                                <span class="author">Mark Twain</span>
-                            </p>
-                        </div>
-                    </div>
-                    <div class="book">
-                        <div class="cover">
-                            <img src="http://talkingwriting.com//sites/default/files/Bird-by-Bird-image1.jpg">
-                        </div>
-                        <div class="description">
-                            <p class="title">Bird By Bird<br>
-                                <span class="author">Anne Lamott</span>
-                            </p>
-                        </div>
-                    </div>
-                    <div class="book">
-                        <div class="cover">
-                            <img src="http://d.gr-assets.com/books/1414348859l/23209971.jpg">
-                        </div>
-                        <div class="description">
-                            <p class="title">Girl at War<br>
-                                <span class="author">Sara Novic</span>
-                            </p>
-                        </div>
-                    </div>
-                    <div class="book">
-                        <div class="cover">
-                            <img src="http://prodimage.images-bn.com/pimages/9780062315007_p0_v2_s192x300.jpg">
-                        </div>
-                        <div class="description">
-                            <p class="title">The Alchemist<br>
-                                <span class="author">Paulo Coelho</span>
-                            </p>
-                        </div>
-                    </div>
-                    <div class="book">
-                        <div class="cover">
-                            <img src="http://eastchapelhillobserver.com/wp-content/uploads/2015/02/amazondotcom.jpg">
-                        </div>
-                        <div class="description">
-                            <p class="title">Anne of Green Gables<br>
-                                <span class="author">Lucy Maud Montgomery</span>
-                            </p>
-                        </div>
-                    </div>
-                    <div class="book">
-                        <div class="cover">
-                            <img src="http://www.penguinbooksindia.com/sites/default/files/book_image/9780143416319.jpg">
-                        </div>
-                        <div class="description">
-                            <p class="title">Animal Farm<br>
-                                <span class="author">George Orwell</span>
-                            </p>
-                        </div>
-                    </div>
-
+                    </div>-->
             </div>
         </div>
     </div>
