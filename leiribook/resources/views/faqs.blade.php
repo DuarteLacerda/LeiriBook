d3812ebb9a8bae7b4fdcb3e16b30e649f257deb0
Duarte <duartelacerda4rgmail.com>
2023-12-14 18:35:11 +0000

    faqs update

--- leiribook/resources/views/faqs.blade.php
+++ leiribook/resources/views/faqs.blade.php
@@ -3,26 +3,24 @@
 
 @section('content')
 <div class="container" id="faq-container" style="margin-top: 5rem">
-    @foreach ($faqs as $faq)
+    @foreach ($faqs as $key => $faq)
     <div class="card mb-3" style="display: none;">
         <div class="card-header d-flex justify-content-between align-items-center">
             <h5 class="mb-0">
                 {{ $faq->question }}
             </h5>
-            <button class="btn" data-toggle="collapse" data-target="#collapse1" aria-expanded="true"
-                aria-controls="collapse1">
+            <button class="btn" data-toggle="collapse" data-target="#collapse{{ $key }}" aria-expanded="true"
+                aria-controls="collapse{{ $key }}">
                 <i class="fas fa-chevron-down"></i>
             </button>
         </div>
-        <div id="collapse1" class="collapse">
+        <div id="collapse{{ $key }}" class="collapse">
             <div class="card-body">
                 {{ $faq->answer }}
             </div>
         </div>
     </div>
     @endforeach
-
-
 </div>
 @endsection
 
