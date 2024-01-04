8a9e17640b96e4d84134d305711763b429599a66
daniel <danielribeiro290404@hotmail.com>
2024-01-04 10:59:35 +0000

    criada pagina do livro

---
+++ leiribook/resources/views/livro_detalhe.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.master')
+@section('title', 'LeiriBook - Biblioteca')
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('css/ribeiro.css') }}">
+@endsection
+
+@section('content')
+<!--xd-->
+@endsection
