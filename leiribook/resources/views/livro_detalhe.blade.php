2b73b482c2756ff3be9ad6db4fada5caa5d1b06d
daniel <danielribeiro290404@hotmail.com>
2024-01-05 12:35:49 +0000

    design decente da pagina livrosdetalhes

--- leiribook/resources/views/livro_detalhe.blade.php
+++ leiribook/resources/views/livro_detalhe.blade.php
@@ -6,25 +6,26 @@
 
 @section('content')
     <!--xd-->
-    <h1>{{ $livroTitulo }} </h1>
+    <h1 id="booktitle">{{ $livroTitulo }} </h1>
     <div id="bookcontainer">
         <div id="bookimage">
             <img src="{{ asset('storage/books/' . $livroFoto) }}" alt="Imagem do Livro">
         </div>
         <div id="bookinfo">
-            <p><strong>Autor:</strong> {{ $livroAutor }}</p>
-            <p><strong>Edição:</strong> {{ $livroEdicao }}</p>
+            <p><strong>Autor:</strong> {{ $livroAutor }}<br>
+                <strong>Edição:</strong> {{ $livroEdicao }}<br>
 
 
-            <h3>Categorias:</h3>
-            @if (count($categorias) > 0)
-                <ul>
+                <strong>Categorias:</strong>
+                @if (count($categorias) > 1)
                     @foreach ($categorias as $categoria)
-                        <li>{{ $categoria->nome }}</li>
-                    @endforeach
-                </ul>
-            @else
-                <p>Este livro não tem categorias associadas.</p>
+                        {{ $categoria->nome }},
+            </p>
+            @endforeach
+        @else
+            @foreach ($categorias as $categoria)
+                {{ $categoria->nome }}</p>
+            @endforeach
             @endif
 
 
