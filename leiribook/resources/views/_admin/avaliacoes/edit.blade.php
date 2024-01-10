f0f97498697f8baf7f719c672cdda525247b6646
vanessaramalho <2221009@my.ipleiria.pt>
2024-01-10 19:12:37 +0000

    atualicazao pag avsliacoes

--- leiribook/resources/views/_admin/avaliacoes/edit.blade.php
+++ leiribook/resources/views/_admin/avaliacoes/edit.blade.php
@@ -23,11 +23,11 @@
             <span>/</span>
         </li>
         <li class="list-inline-item active">
-            <a href="{{ route('admin.avaliacoes.edit', $avaliacoes) }}">Editar</a>
+            <a href="{{ route('admin.avaliacoes.edit', $avaliacao) }}">Editar</a>
         </li>
     </ul>
 </div>
-<button onclick="location.href='{{ route('admin.faqs.index') }}';" class="au-btn au-btn-icon au-btn--green">
+<button onclick="location.href='{{ route('admin.avaliacoes.index') }}';" class="au-btn au-btn-icon au-btn--green">
     <i class="fa fa-arrow-left"></i>Voltar
 </button>
 @endsection
@@ -43,46 +43,16 @@
                             <h3 class="text-center title-2">Editar</h3>
                         </div>
                         <hr>
-                        <form id="submit" action="{{ route('admin.faqs.update', $faq) }}" method="post">
+                        <form id="submit" action="{{ route('admin.avaliacoes.update', $avaliacao) }}" method="post">
                             @csrf
                             @method('PUT')
-                            <div class="form-group">
-                                <label for="question" class="control-label mb-1">Pergunta:</label>
-                                <input id="question" name="question" type="text" class="form-control"
-                                    aria-required="true" aria-invalid="false"
-                                    value="{{ old('question', $faq->question) }}">
-                            </div>
-                            <div class="row">
-                                <div class="col-6">
-                                    <div class="form-group">
-                                        <label for="answer" class="control-label mb-1">Resposta:</label>
-                                        <textarea id="answer" name="answer" type="text" class="form-control cc-exp"
-                                            data-val="true" placeholder="Estado"
-                                            rows="5">{{ old('answer', $faq->answer) }}</textarea>
-                                    </div>
+                            @include('_admin.avaliacoes.partials.add-edit')
+                                <div>
+                                    <button id="payment-button" type="submit" class="btn btn-lg btn-info btn-block">
+                                        <i class="fa fa-plus fa-xl"></i>&nbsp;
+                                        <span id="payment-button-amount">Editar</span>
+                                    </button>
                                 </div>
-                                <div class="col-6">
-                                    <label for="state" class="control-label mb-1">Estado</label>
-                                    <div class="input-group">
-                                        <select name="state" id="state" class="form-control">
-                                            <option value="">Selecione um estado</option>
-                                            <option value="0" {{ (old('state', $faq->approved) == 0) ? 'selected' : ''
-                                                }}>Pendente
-                                            </option>
-                                            <option value="1" {{ (old('state', $faq->approved) == 1) ? 'selected' : ''
-                                                }}>Aprovado
-                                            </option>
-                                        </select>
-
-                                    </div>
-                                </div>
-                            </div>
-                            <div>
-                                <button id="payment-button" type="submit" class="btn btn-lg btn-info btn-block">
-                                    <i class="fa fa-edit fa-xl"></i>&nbsp;
-                                    <span id="payment-button-amount">Editar</span>
-                                </button>
-                            </div>
                         </form>
                     </div>
                 </div>
