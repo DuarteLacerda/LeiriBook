b393cad0c32de6521c2d1c3378b786c698d55591
Duarte <duartelacerda4rgmail.com>
2024-01-18 14:18:36 +0000

    Video Promocional

--- leiribook/resources/views/welcome.blade.php
+++ leiribook/resources/views/welcome.blade.php
@@ -7,60 +7,69 @@
 @section('content')
 
 <button onclick="topFunction()" id="myBtn" title="Ir para cima"><i class="fa-solid fa-arrow-up"></i></button>
-<div class="sec1">
-    <div class="sec1_title">
-        <h1>Leiribook</h1>
-        <h2>Ler para Crescer</h2>
+<div class="cont">
+    <div class="sec1">
+        <div class="sec1_title">
+            <h1>Leiribook</h1>
+            <h2>Ler para Crescer</h2>
+        </div>
+        <a href="#sec2">
+            <div class='icon-scroll'></div>
+        </a>
     </div>
-    <a href="#sec2">
-        <div class='icon-scroll'></div>
-    </a>
-</div>
-<div class="sec2" id="sec2">
-    <div class="slider-wrapper">
-        <div class="slider">
-            @foreach ($eventos as $evento)
-            <div class="slide">
-                @if ($evento->fotos->isNotEmpty())
-                <img src="{{ asset('storage/eventos_fotos/'.$evento->fotos->first()->foto) }}" alt="">
-                @else
-                <img src="{{ asset('images/admin/default-user.png') }}" alt="" />
-                @endif
-                <div class="overlay">
-                    <span class="hover-title">{{ $evento->nome }}</span>
-                    <span class="hover-desc">{{ $evento->descricao }}</span>
+    <div class="sec4">
+        <div class="secVid">
+            <video autoplay loop controls controlsList="nodownload">
+                <source src="{{ asset('images/promoVideo.mp4') }}" type="video/mp4">
+            </video>
+        </div>
+    </div>
+    <div class="sec2" id="sec2">
+        <div class="slider-wrapper">
+            <div class="slider">
+                @foreach ($eventos as $evento)
+                <div class="slide">
+                    @if ($evento->fotos->isNotEmpty())
+                    <img src="{{ asset('storage/eventos_fotos/'.$evento->fotos->first()->foto) }}" alt="">
+                    @else
+                    <img src="{{ asset('images/admin/default-user.png') }}" alt="" />
+                    @endif
+                    <div class="overlay">
+                        <span class="hover-title">{{ $evento->nome }}</span>
+                        <span class="hover-desc">{{ $evento->descricao }}</span>
+                    </div>
+                </div>
+                @endforeach
+                <button id="prev"><i class="fa-solid fa-chevron-right fa-rotate-180"></i></button>
+                <button id="next"><i class="fa-solid fa-chevron-right"></i></button>
+            </div>
+        </div>
+    </div>
+    <div class="sec3">
+        <div class="cards">
+            @foreach ($livros as $livro)
+            <div class="card">
+                <div class="image" id="image">
+                    @if ($livro->foto != null)
+                    <img src="{{ asset('storage/books/'.$livro->foto) }}" alt="">
+                    @else
+                    <img src="{{ asset('images/admin/default-user.png') }}" alt="" />
+                    @endif
+                </div>
+                <div class="content">
+                    <span class="title">
+                        {{ $livro->titulo }}
+                    </span>
+                    <a class="action" href="#">
+                        Saber mais
+                        <span aria-hidden="true">
+                            <i class="fa-solid fa-arrow-right-long"></i>
+                        </span>
+                    </a>
                 </div>
             </div>
             @endforeach
-            <button id="prev"><i class="fa-solid fa-chevron-right fa-rotate-180"></i></button>
-            <button id="next"><i class="fa-solid fa-chevron-right"></i></button>
         </div>
-    </div>
-</div>
-<div class="sec3">
-    <div class="cards">
-        @foreach ($livros as $livro)
-        <div class="card">
-            <div class="image" id="image">
-                @if ($livro->foto != null)
-                <img src="{{ asset('storage/books/'.$livro->foto) }}" alt="">
-                @else
-                <img src="{{ asset('images/admin/default-user.png') }}" alt="" />
-                @endif
-            </div>
-            <div class="content">
-                <span class="title">
-                    {{ $livro->titulo }}
-                </span>
-                <a class="action" href="#">
-                    Saber mais
-                    <span aria-hidden="true">
-                        <i class="fa-solid fa-arrow-right-long"></i>
-                    </span>
-                </a>
-            </div>
-        </div>
-        @endforeach
     </div>
 </div>
 @endsection
